fbe1c06653838857cde2a01932d7c0e923238a75
Keshav <72556571+classPythonAddike@users.noreply.github.com.>
2026-08-16 20:18:15 +0530

    Added GDS parser

--- schema.py
+++ schema.py
@@ -3,13 +3,36 @@ from pydantic import BaseModel
 
 from typing import List, Dict
 
+# Map SkyWater 130 GDS layer numbers to metal layer names
+LAYER_MAP = {
+    66: {"MET": "poly", "VIA": "licon"},
+    67: {"MET": "li1", "VIA": "mcon"},
+    68: {"MET": "met1", "VIA": "via"},
+    69: {"MET": "met2", "VIA": "via2"},
+    70: {"MET": "met3", "VIA": "via3"},
+    71: {"MET": "met4", "VIA": "via4"},
+    72: {"MET": "met5"},
+}
+
+MET_DATATYPE = 20
+VIA_DATATYPE = 44
+
 class Coordinate(BaseModel):
     x: int
     y: int
 
+    def __add__(self, other):
+        return Coordinate(x=(self.x + other.x), y=(self.y + other.y))
+
+    def __sub__(self, other):
+        return Coordinate(x=(self.x - other.x), y=(self.y - other.y))
+
+    def __floordiv__(self, other):
+        return Coordinate(x=self.x // other, y=self.y // other)
+
 class Pin(BaseModel):
     name: str
-    center: Coordinate # Relative to the bottom-left coordinate of the cell
+    centers: List[Coordinate] # Relative to the bottom-left coordinate of the cell
     net: str
 
 class STDCell(BaseModel):
@@ -25,13 +48,14 @@ class Polygon(BaseModel):
     layer: str
     net: str
 
-class Vias(BaseModel):
+class Via(BaseModel):
     center: Coordinate
     layer: str
     net: str
 
 class DB(BaseModel):
+    units: float # DB units
     std_cells: Dict[str, STDCell] # name: Cell()
     polygons: List[Polygon]
-    vias: List[Vias]
+    vias: List[Via]
 
